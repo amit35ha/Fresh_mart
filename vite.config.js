0f4b2c00b7c1ef9239dbbe87cb7d26a2fc284f1e
amit35ha <amitkumar30072006@gmail.com>
2026-09-24 16:03:05 +0530

    Initial commit of Fresh_mart full-stack app

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://fresh-mart-1-x8e6.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         secure: false
       }
